b5327447e7ec5c231bbd7d4c197051a14254f771
Amr Ezzat <3mr3zzat@gmail.com>
2020-03-06 21:34:06 +0200

    [API] Search hotels Endpoint

---
+++ app/Http/Controllers/Api/HotelsController.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class HotelsController extends Controller
+{
+
+    private $hotelsProviders;
+
+
+    public function __construct(array $hotelsProviders)
+    {
+        $this->hotelsProviders = $hotelsProviders;
+    }
+
+    public function list(Request $request): JsonResponse
+    {
+        $hotels = [];
+        foreach ($this->hotelsProviders as $hotelProvider) {
+
+            $hotels = array_merge($hotels, $hotelProvider->search($request));
+        }
+        $this->sortByRate($hotels);
+
+        return response()->json($hotels, 200);
+    }
+
+    private function sortByRate(&$hotels)
+    {
+        $rate = array_column($hotels, 'rate');
+
+        array_multisort($rate, SORT_DESC, $hotels);
+
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Services\HotelsProviderB;
+use App\Http\Controllers\Api\HotelsController;
 use Illuminate\Support\ServiceProvider;
+use App\Services\HotelsProviderA;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bind('hotelsProviders', function ($app) {
+            return [
+                $this->app->make(HotelsProviderA::class),
+                $this->app->make(HotelsProviderB::class),
+            ];
+        });
+
+        $this->app->bind(HotelsController::class, function () {
+            return new HotelsController($this->app->make('hotelsProviders'));
+        });
     }
 }
